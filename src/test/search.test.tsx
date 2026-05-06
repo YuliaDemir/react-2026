b8f57bbf8eaa56d91d23b14ab1bd1c6b92415528
Yulia  Demir <Nightelfin91@gmail.com>
2026-05-06 07:36:16 +0300

    feat: add Rendering Tests

---
+++ src/test/search.test.tsx
@@ -0,0 +1,53 @@
+import { render, screen } from "@testing-library/react";
+import Search from "../components/search";
+import userEvent from '@testing-library/user-event';
+
+describe('Search component ', () => {
+    it('renders search input and search button', () => {
+        const mockOnSearch = vi.fn();
+        const text = 'Test search';
+
+        render(<Search onSearch={mockOnSearch} value={text} />);
+
+        const searchInput = screen.getByRole('textbox');
+        const searchButton = screen.getByRole('button', { name: /search/i });
+
+        expect(searchInput).toBeInTheDocument();
+        expect(searchButton).toBeInTheDocument();
+    })
+
+    it('Displays previously saved search term from localStorage on mount', () => {
+        localStorage.setItem('query', 'Pikachu');
+
+        const mockOnSearch = vi.fn();
+        const text = 'Test search';
+
+        render(<Search onSearch={mockOnSearch} value={text} />);
+
+        expect(screen.getByRole('textbox')).toHaveValue('Pikachu');
+        localStorage.removeItem('query');
+    })
+
+    it('Shows empty input when no saved term exists', () => {
+        localStorage.removeItem('query');
+
+        const mockOnSearch = vi.fn();
+        const text = 'Test search';
+
+        render(<Search onSearch={mockOnSearch} value={text} />);
+        expect(screen.getByRole('textbox')).toHaveValue('');
+    })
+
+    it('Updates input value when user types', async () => {
+        const user = userEvent.setup();
+        const mockOnSearch = vi.fn();
+
+        render(<Search onSearch={mockOnSearch} value="" />);
+        const input = screen.getByRole('textbox');
+
+        await user.type(input, 'Bulbasaur');
+        expect(input).toHaveValue('Bulbasaur');
+    })
+
+    
+})
