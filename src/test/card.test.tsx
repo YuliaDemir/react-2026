392d32646d90f5ecb283bd22cd5e50f25803ab6d
Yulia  Demir <Nightelfin91@gmail.com>
2026-05-07 11:16:49 +0300

    feat: add  Card/Item Component Tests

---
+++ src/test/card.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react'
+import Card from '../components/card'
+
+describe('Card', () => {
+    it('Displays item name and description correctly', () => {
+        render(<Card name="Bulbasaur" description="Grass/Poison Pokémon" />)
+
+        expect(screen.getByText('Bulbasaur')).toBeInTheDocument()
+        expect(screen.getByText('Grass/Poison Pokémon')).toBeInTheDocument()
+    })
+
+})
