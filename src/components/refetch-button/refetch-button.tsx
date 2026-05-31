faf5edc1f949cd428e033212cb5f42c01fc12c05
Yulia  Demir <Nightelfin91@gmail.com>
2026-05-31 14:51:21 +0300

    refactor: move functions from refetch button to the dedicated hook

--- src/components/refetch-button/refetch-button.tsx
+++ src/components/refetch-button/refetch-button.tsx
@@ -1,53 +1,10 @@
-import { useState, type FocusEvent } from "react";
-
-import {
-    productFetchSlice,
-    ProductsApiTagsAndIds,
-} from "@/api/products/products-fetch-slice";
-import { useAppDispatch } from "@/store/hook";
 import { ButtonOrLink } from "../button/button-or-link";
 
 import styles from "./refetch-button.module.scss";
-import { useDetalisation } from "@/utils/hooks/use-detalisation";
+import { useStateRefetchButton } from "@/utils/hooks/use-handle-refetch";
 
 export const RefetchButton = () => {
-    const dispatch = useAppDispatch();
-    const { searchParams, detailsId } = useDetalisation();
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const closeMenu = () => {
-        setIsMenuOpen(false);
-    };
-
-    const handleRefetch = (type: "all" | "current") => {
-        const page = Number(searchParams.get("page") ?? 1) || 1;
-        const query = searchParams.get("q")?.trim() ?? "";
-
-        const currentTagIdForPage = query
-            ? ProductsApiTagsAndIds.ids.listPerPageAndQuery(page, query)
-            : ProductsApiTagsAndIds.ids.listPerPage(page);
-
-        const tagIdList = type === "all" ?
-            [ProductsApiTagsAndIds.ids.all] :
-            [currentTagIdForPage, ...(detailsId ? [Number(detailsId)] : [])];
-
-        dispatch(
-            productFetchSlice.util.invalidateTags(tagIdList.map(tag =>
-            ({
-                type: ProductsApiTagsAndIds.tags.products,
-                id: tag,
-            })
-            ))
-        );
-
-        closeMenu();
-    };
-
-    const handleBlur = (event: FocusEvent<HTMLDivElement>) => {
-        if (!event.currentTarget.contains(event.relatedTarget)) {
-            closeMenu();
-        }
-    };
+    const { isMenuOpen, setIsMenuOpen, handleBlur, handleRefetch } = useStateRefetchButton();
 
     return (
         <div className={styles.root} onBlur={handleBlur}>
